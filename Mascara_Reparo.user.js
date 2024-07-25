c2bfa7d6ccca16d45c14ac64fb88fc2f8b45e7f7
LowDepth <marcelobts359@gmail.com>
2024-07-25 12:25:55 -0300

    Update Mascara_Reparo.user.js

--- Mascara_Reparo.user.js
+++ Mascara_Reparo.user.js
@@ -8,10 +8,11 @@
 // @match       http://operacao-sisweb/Unificado/stavancado/Tratativa/Tratar*
 // @match       http://10.121.245.200/stu/site/prioridades-cl/cadastrar/*
 // @grant       none
-// @version     1.9.8.1
+// @version     1.9.8.2
 // @author      Marcelo B
-// @updateURL	https://github.com/LowDepth/Mascrep/raw/main/Mascara_Reparo.user.js
+// @updateURL	  https://github.com/LowDepth/Mascrep/raw/main/Mascara_Reparo.user.js
 // @downloadURL	https://github.com/LowDepth/Mascrep/raw/main/Mascara_Reparo.user.js
+// @icon        https://raw.githubusercontent.com/LowDepth/Mascrep/main/c66f3775-f455-4ed1-8f6c-5d497e95940f.jfif
 // ==/UserScript==
 
 if (location.hostname === '10.121.245.200') {
